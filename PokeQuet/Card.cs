4276dfc4890bf16d73a16ec16f70cbb241a37b22
Tim Braumann <t.braumann@rmh-media.com>
2018-05-27 22:52:52 +0200

    initial commit. empty project files.

---
+++ PokeQuet/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PokeQuet
+{
+    public class Card
+    {
+        public string name, flavorText;
+        public int hp, atk, def, spd;
+
+        public Card()
+        {
+        }
+
+
+    }
+}
---
+++ PokeQuet/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+using PokeQuet;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        var eevee = new Card
+        {
+            name = "Eeveee"
+        };
+        this.label3.Text = eevee.name;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
