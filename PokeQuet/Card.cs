8034cb7aaf9183bc31432ef2c89ddda91b4df43a
Necro <Kramp_A@web.de>
2018-05-30 17:36:03 +0200

    Rough structure+ class model

--- PokeQuet/Card.cs
+++ PokeQuet/Card.cs
@@ -1,16 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PokeQuet
 {
     public class Card
     {
-        public string name, flavorText;
+        public string name, flavorText, texture, type;
         public int hp, atk, def, spd;
+    }
 
-        public Card()
+    public class Deck : LinkedList<Card> {
+
+        public Card GetCurrentCard()
         {
+            return this.First.Value;
         }
 
+        public void PutCardAtBack(Card card)
+        {
 
+        }
+
+        public static void FillDecksFromCardPool(IEnumerable<Card> pool, Deck deck1, Deck deck2)
+        {
+            deck1.Clear();
+            deck2.Clear();
+            //TODO: Kartenmischlogik hier
+        }
+    }
+
+    public class Player{
+        public Deck Deck { get; } = new Deck();
+        public string Name { get; set; }
+
+        public Player(string name) {
+            this.Name = name;
+        }
+
+        //TODO: Abgrenzen von CPU und menschlichen Spielern
     }
 }
--- PokeQuet/MainWindow.cs
+++ PokeQuet/MainWindow.cs
@@ -4,19 +4,61 @@ using PokeQuet;
 
 public partial class MainWindow : Gtk.Window
 {
+
+    public Player ActivePlayer { get; set; }
+    public Deck TieCards { get; set; }
+
     public MainWindow() : base(Gtk.WindowType.Toplevel)
     {
         Build();
-        var eevee = new Card
-        {
-            name = "Eeveee"
-        };
-        this.label3.Text = eevee.name;
     }
+
+    public void InitGame()
+    {
+
+    }
+
+    public void LoadCards()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+    //TODO: Auswahl der Disziplin durch Event-Callback bei Klick
+
+    public void MakeCPUMove()
+    {
+
+    }
+
+    //TODO: Vielleicht Enum für Disziplin benutzen
+    public void CompareDiscipline(int discipline)
+    {
+
+    }
+
+    public void RoundDecided(Player winningPlayer, Player losingPlayer)
+    {
+
+    }
+
+    public void CheckWinningState()
+    {
+
+    }
+
+
+    #region Autogeneriert
 
     protected void OnDeleteEvent(object sender, DeleteEventArgs a)
     {
         Application.Quit();
         a.RetVal = true;
     }
+
+    #endregion
 }
